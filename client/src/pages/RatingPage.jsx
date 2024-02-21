511499d4a9480ad00d7785d7b6b668c807fa2083
KostasBzn <kostasbouzianis@gmail.com>
2024-02-21 11:11:11 +0100

    rating page fine tuning

--- client/src/pages/RatingPage.jsx
+++ client/src/pages/RatingPage.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useContext } from "react";
 import { RestaurantContext } from "../context/restaurantContext";
 import { useAuthContext } from "../context/authContext";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const RatingPage = () => {
   const { addNewRating } = useContext(RestaurantContext);
@@ -86,9 +86,9 @@ const RatingPage = () => {
             </div>
           </form>
           <div className="h-20 flex items-center justify-center">
-            <a href="#" className="text-gray-600">
+            <Link className="text-gray-600" to="/">
               Maybe later
-            </a>
+            </Link>
           </div>
         </div>
       </div>
