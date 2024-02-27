7572700a8075b3cfa566e8d46d4e0c080bb8262c
KostasBzn <kostasbouzianis@gmail.com>
2024-02-27 13:52:57 +0100

    favourite button fixed

--- client/src/pages/DescriptionPage.jsx
+++ client/src/pages/DescriptionPage.jsx
@@ -23,6 +23,7 @@ function DescriptionPage() {
       user?.favourites.map((item) => item._id).includes(restaurantId) ||
         user?.favourites.includes(restaurantId)
     );
+    console.log("user==>>", user);
   }, [user]);
 
   return (
@@ -30,7 +31,7 @@ function DescriptionPage() {
       <h2 className=" text-6xl font-bold">{restaurant?.name}</h2>
       <div className=" flex gap-5 text-lg font-bold mt-5 ">
         <p> {restaurant?.category} cuisine</p>
-        <div className=" flex justify-center items-center gap-3  ">
+        <div className=" flex justify-center items-center ">
           <svg
             className={`w-7 h-7 text-yellow-500   `}
             xmlns="http://www.w3.org/2000/svg"
@@ -61,7 +62,7 @@ function DescriptionPage() {
 
       <div className=" bg-amber-100 flex flex-col mt-10">
         <h3 className="font-bold mb-2 text-4xl ml-2">Menu</h3>
-        <div className="px-6 py-4 mt-1  grid grid-cols-3  gap-6">
+        <div className="px-6 py-4 mt-5  grid grid-cols-3  gap-6">
           {restaurant?.menu.map((item) => (
             <MenuItemCard key={item._id} item={item} />
           ))}
@@ -70,32 +71,20 @@ function DescriptionPage() {
       <hr className=" mt-20 border-black" />
 
       <div className=" ml-5 flex justify-center  items-center mt-36 gap-40">
-        <div className=" w-1/3 flex gap-20 ">
+        <div className=" w-1/2 flex gap-20 ">
           <p>{restaurant?.info}</p>
         </div>
-        <div className=" w-1/3 text-lg  flex gap-20">
+        <div className=" w-1/4 text-lg  flex gap-20">
           <div className=" flex flex-col">
-            {/* <h3 className=" font-semibold">Address:</h3>
+            <h3 className=" font-semibold">Address:</h3>
             <p>Street: {restaurant?.address.street}</p>
             <p>
               City: {restaurant?.address.city} {restaurant?.address.postalCode}
             </p>
-            <p>Country: {restaurant?.address.country}</p> */}
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3309.384209086383!2d-118.24111842318315!3d33.95696297319333!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c96fa4433035%3A0x2aebe07feee6cff7!2sElm%20St%2C%20Los%20Angeles%2C%20CA%2090001%2C%20USA!5e0!3m2!1sen!2sde!4v1709033509902!5m2!1sen!2sde"
-              width="600"
-              height="450"
-              style={{border:0}}
-              allowfullscreen=""
-              loading="lazy"
-              referrerpolicy="no-referrer-when-downgrade"
-            >
-              {" "}
-              See map
-            </iframe>
+            <p>Country: {restaurant?.address.country}</p>
           </div>
         </div>
-        <div className=" w-1/3 text-lg gap-20 flex">
+        <div className=" w-1/4 text-lg gap-20 flex">
           <div className=" flex flex-col">
             <p className="font-bold">Delivery times</p>
             <p>
